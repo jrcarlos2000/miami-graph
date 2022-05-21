ce6a75f67e3bdfb7377fe53724c9e8e65728951f
Xavier Xia <boxia@andrew.cmu.edu>
2022-05-21 13:12:06 -0400

    added doi schema and entity

--- src/mapping.ts
+++ src/mapping.ts
@@ -5,17 +5,43 @@ import {
 import {
   mintItemEvent
 } from "../generated/Contract/Contract"
-import { UserNFT } from "../generated/schema"
+import { UserNFT, doiNFT } from "../generated/schema"
 
+
+// adding the index of each NFT to its user (entity)
 export function handleMintItem(
   event: mintItemEvent
 ): void {
-  let entity = UserNFT.load(event.transaction.from.toHex())
-
+  let entity = UserNFT.load(event.transaction.from.toHex());
   if (!entity) {
     entity = new UserNFT(event.transaction.from.toHex())
     entity.indexes = [event.params._index];
+    entity.save()
+    
+    entity.save()
   }
-  entity.indexes = [event.params._index];
-  entity.save();
+  else{
+    let tmp = entity.indexes;
+    tmp.push(event.params._index);
+    entity.indexes = tmp;
+    entity.save();
+  }
+  //doi
+
+  let doiEntity = doiNFT.load(event.params._doi);
+  if (!doiEntity) {
+    doiEntity = new doiNFT(event.params._doi);
+    doiEntity.indexes = [event.params._index];
+    doiEntity.save()
+  }
+  else{
+    let tmp = doiEntity.indexes;
+    tmp.push(event.params._index);
+    doiEntity.indexes = tmp;
+    doiEntity.save();
+  }
 }
+
+
+// adding the index of each NFT to its doi
+
