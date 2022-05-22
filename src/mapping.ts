a4f7e1bd84d76941c684c82b1bfeefcc71c2ca4d
Xavier Xia <boxia@andrew.cmu.edu>
2022-05-22 00:47:05 -0400

    edited mapping to new proposalvault

--- src/mapping.ts
+++ src/mapping.ts
@@ -6,6 +6,10 @@ import {
   mintItemEvent
 } from "../generated/Contract/Contract"
 import { UserNFT, doiNFT } from "../generated/schema"
+import {
+  proposalMade
+} from "../generated/ProposalContract/Contract"
+import { UserProposal, doiProposal } from "../generated/schema"
 
 
 // adding the index of each NFT to its user (entity)
@@ -40,19 +44,42 @@ export function handleMintItem(
     doiEntity.indexes = tmp;
     doiEntity.save();
   }
-  // proposals
-  // let proposalEntity = doiNFT.load(event.params._doi);
-  // if (!doiEntity) {
-  //   doiEntity = new doiNFT(event.params._doi);
-  //   doiEntity.indexes = [event.params._index];
-  //   doiEntity.save()
-  // }
-  // else{
-  //   let tmp = doiEntity.indexes;
-  //   tmp.push(event.params._index);
-  //   doiEntity.indexes = tmp;
-  //   doiEntity.save();
-  // }
+}
+// adding the index of each NFT to its user (entity)
+// event proposalMade(
+//   string doi,
+//   address proposer,
+//   uint256 idx,
+//   uint256 deadline
+// );
+export function ProposalMadeItem(
+  event: proposalMade
+): void {
+  let entity = UserProposal.load(event.transaction.from.toHex());
+  if (!entity) {
+    entity = new UserProposal(event.transaction.from.toHex())
+    entity.indexes = [event.params.idx];
+    entity.save()
+  }
+  else{
+    let tmp = entity.indexes;
+    tmp.push(event.params.idx);
+    entity.indexes = tmp;
+    entity.save();
+  }
+
+  let doiEntity = doiProposal.load(event.params.doi);
+  if (!doiEntity) {
+    doiEntity = new doiProposal(event.params.doi);
+    doiEntity.indexes = [event.params.idx];
+    doiEntity.save()
+  }
+  else{
+    let tmp = doiEntity.indexes;
+    tmp.push(event.params.idx);
+    doiEntity.indexes = tmp;
+    doiEntity.save();
+  }
 }
 
 
